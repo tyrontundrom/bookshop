6c93a333dff0beabdd81f85003dc0b352274af37
ThinkPad <tyrontundrom@gmail.com>
2021-11-01 14:08:29 +0100

    wstrzykiwanie wartości prymitywnych

--- src/main/java/pl/tyrontundrom/bookShop/ApplicationStartup.java
+++ src/main/java/pl/tyrontundrom/bookShop/ApplicationStartup.java
@@ -1,6 +1,6 @@
 package pl.tyrontundrom.bookShop;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.tyrontundrom.bookShop.catalog.domain.Book;
@@ -9,14 +9,23 @@ import pl.tyrontundrom.bookShop.catalog.domain.CatalogService;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
 
     private final CatalogService catalogController;
+    private final String title;
+    private final Long limit;
+
+    public ApplicationStartup(CatalogService catalogController,
+                              @Value("${bookShop.catalog.query}") String title,
+                              @Value("${bookShop.catalog.limit:2}") Long limit) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
 
     @Override
     public void run(String... args) {
-        List<Book> books = catalogController.findByTitle("Pan");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
